1fbe0a7d8e520e4895c662a7e184898a41bf1b53
FanJups <jupsfan@gmail.com>
2019-12-23 00:51:12 +0100

    Update manage.component.ts

--- src/app/components/manage/manage.component.ts
+++ src/app/components/manage/manage.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { StocksService } from '../../services/stocks.service';
 
 @Component({
   selector: 'manage',
   templateUrl: './manage.component.html',
   styleUrls: ['./manage.component.css']
 })
-export class ManageComponent implements OnInit {
+export class ManageComponent  {
 
-  constructor() { }
+  symbols: Array<string>;
 
-  ngOnInit() {
-  }
+  stock: string;
+constructor(private service: StocksService) {
+this.symbols = service.get();
+}
+add() {
+this.symbols = this.service.add(this.stock.toUpperCase());
+this.stock = '';
+}
+remove(symbol) {
+this.symbols = this.service.remove(symbol);
+}
+
+  
 
 }
